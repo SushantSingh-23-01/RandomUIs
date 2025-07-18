49543b71d35073e8a21cf53a5c9d40de87b2d64f
Sushant Singh <151197353+SushantSingh-23-01@users.noreply.github.com>
2025-07-18 18:53:04 +0530

    Update chatuiV2.py

--- Chat_Uis/chatuiV2.py
+++ Chat_Uis/chatuiV2.py
@@ -1,5 +1,4 @@
 import gradio as gr
-import random
 import json
 import ollama
 from datetime import datetime
@@ -7,29 +6,44 @@ import os
 
 class ChatUI:
     def __init__(self):
-        # Default Parameters
-        self.default_chats_directory = os.path.join(
-            os.path.dirname(os.path.realpath(__file__)),
-            'chats'
-        )
-        os.makedirs(self.default_chats_directory, exist_ok=True)
-        self.default_system_prompt = 'Act as an assistant.'
-        
-        # State Parameters
-        self.system_prompt_state = gr.State('Act as an assistant')
         self.available_models = [i['model'] for i in ollama.list()['models']]
-        default_model = self.available_models[0] if self.available_models else ''
-        self.model_name_state = gr.State(default_model)
+        self.default_model = self.available_models[0] if self.available_models else ''
+        
+        self.default_system_prompt = 'Act as an assistant.'
+
+        self.default_chats_directory: str = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'chats')
+        os.makedirs(self.default_chats_directory, exist_ok=True)
+        
         self.chat_history_state = gr.State([])
         
         self._interface()
         self._events()   
-    
-    def response(self, message, chat_history, system_prompt, model_name):
-        chat_history.append({"role": "user", "content": message}) 
-        messages = [{'role':'system','content':system_prompt}] + chat_history
-        bot_message = ollama.chat(model = model_name, messages=messages)['message']['content']    
-        chat_history.append({"role": "assistant", "content": bot_message})
+
+    def response(
+        self, 
+        message, 
+        chat_history, 
+        system_prompt, 
+        model_name,
+        temperature, 
+        top_k, 
+        top_p
+        ):
+        try:
+            chat_history.append({"role": "user", "content": message}) 
+            messages = [{'role':'system','content':system_prompt}] + chat_history
+            bot_message = ollama.chat(
+                model = model_name, 
+                messages=messages,
+                options={
+                    'temperature': temperature,
+                    'top_k': top_k,
+                    'top_p': top_p
+                }
+                )['message']['content']    
+            chat_history.append({"role": "assistant", "content": bot_message})
+        except ollama.ResponseError as e:
+                    print(f"Ollama Error: {e}")
         return '', chat_history, chat_history
     
     def _undo_last_turn(self, chat_history):
@@ -40,13 +54,13 @@ class ChatUI:
                 chat_history.pop()
         return chat_history, chat_history
     
-    def _regenerate_last_message(self, chat_history, system_prompt, model_name):
+    def _regenerate_last_message(self, chat_history, system_prompt, model_name, temperature, top_k, top_p):
         if len(chat_history) >= 2 and \
             chat_history[-2]['role'] == 'user' and \
             chat_history[-1]['role'] == 'assistant':
             chat_history.pop()
             last_user_message = chat_history.pop()['content']
-            _, chat_history, chat_history= self.response(last_user_message, chat_history, system_prompt, model_name)
+            _, chat_history, chat_history= self.response(last_user_message, chat_history, system_prompt, model_name, temperature, top_k, top_p)
         return '', chat_history, chat_history
     
     def _save_chat(self, chat_history, chat_files_dir, chat_name_input):
@@ -72,8 +86,12 @@ class ChatUI:
             
     def _load_chat(self, chat_files_dir, filename):
         chat_history = []
-        filepath = os.path.join(chat_files_dir, filename)
 
+        if not filename:
+            print("No chat file selected to load.")
+            return [], [], gr.Textbox(value="")
+        
+        filepath = os.path.join(chat_files_dir, filename)
         with open(filepath, "r", encoding='utf-8') as f:
             try:
                 chat_history = json.load(f)
@@ -85,35 +103,37 @@ class ChatUI:
         print(f'System Prompt Set to: {system_prompt}')
         return system_prompt
     
-    def _update_model(self, model_name):
-        print(f'Model Set to: {model_name}')
-        return model_name     
-    
-    def parse_json_files(self, folder_dir):
-        filenames = []
-        for f in os.listdir(folder_dir):
-            if f.endswith("json"):
-                filenames.append(f)
-        return gr.Dropdown(choices=filenames)
+    def _parse_chat_folder(self, chat_files_dir):
+        json_file_paths = []
+        for filename in os.listdir(chat_files_dir):
+            if filename.endswith(".json"):
+                full_path = os.path.join(chat_files_dir, filename)
+                json_file_paths.append(full_path)
 
-    def _create_new_chat(self, new_filename, chat_files_dir):
+        json_file_paths.sort(key=os.path.getmtime, reverse=True)
+
+        filenames = [os.path.basename(path) for path in json_file_paths]
+        return filenames
+
+    def _create_new_chat(self, chat_files_dir, filename):
         chat_history = []
-        if not new_filename: 
+        if not filename: 
             now = datetime.now()
             time_str = now.strftime('%Y-%m-%d-%H-%M-%S')
-            new_filename =  f'Chat_{time_str}'
-        files_list = [f for f in os.listdir(chat_files_dir) if f.endswith('json')]
-        new_files_list = [new_filename] + files_list
-        return chat_history, chat_history, gr.Dropdown(choices=new_files_list, value=new_filename)
+            filename =  f'Chat_{time_str}'
+        updated_files_list = self._parse_chat_folder(chat_files_dir)
+        updated_files_list = [filename] + updated_files_list
+        return chat_history, chat_history, gr.Dropdown(choices=updated_files_list, value=filename), gr.Textbox(value=filename)
     
-    def _delete_chat(self, filename, chat_files_dir, chat_history):
+    def _delete_chat(self, chat_files_dir, filename, chat_history):
         filepath = os.path.join(chat_files_dir, filename)
-        os.remove(filepath)
+        if os.path.exists(filepath):
+            os.remove(filepath)
         if chat_history:
             chat_history = []
             
-        files_list = [f for f in os.listdir(chat_files_dir) if f.endswith('json')]
-        return chat_history, chat_history, gr.Dropdown(choices=files_list), gr.Textbox(value=None)
+        updated_files_list = self._parse_chat_folder(chat_files_dir)
+        return chat_history, chat_history, gr.Dropdown(choices=updated_files_list, value=updated_files_list[0]), gr.Textbox(value=None)
         
     def _interface(self):
         with gr.Tab(label='Chat Inference'):   
@@ -125,7 +145,7 @@ class ChatUI:
                             interactive=True
                         )
                         self.chat_files_dropdown = gr.Dropdown(
-                            choices=[f for f in os.listdir(r'chats') if f.endswith('json')],
+                            choices=self._parse_chat_folder(self.default_chats_directory),
                             label='Chat Files',
                             interactive=True
                         )  
@@ -151,32 +171,58 @@ class ChatUI:
             
         with gr.Tab(label='Settings'):
             with gr.Row():
-                self.chat_files_dir = gr.Textbox(
+                self.chat_files_dir_input = gr.Textbox(
                     value=self.default_chats_directory,
                     label='Chat Export Path',
                     placeholder=r'Enter absolute paths to be on safer side',
-                    interactive=True
+                    interactive=True, 
+                    submit_btn=True
                 )
 
-                self.update_chat_dir_btn = gr.Button(value='Update Chat Directory')
-
-            
             self.system_prompt_input = gr.Textbox(
                 value=self.default_system_prompt,
                 label='LLM System Prompt',
+                interactive=True, 
+                submit_btn=True
             )
+            
             self.model_name_input = gr.Dropdown(
                 choices=self.available_models,
-                value=self.model_name_state.value, 
+                value=self.default_model, 
                 label='Select Ollama Model', 
                 interactive=True 
             )
-            self.update_settings_btn = gr.Button(value='Update Settings')
+            with gr.Row():
+                self.temperature_input = gr.Slider(
+                    minimum=0,
+                    maximum=1.0,
+                    step=0.1,
+                    value=0.9,
+                    label='Temperature',
+                    interactive=True
+                )
+                self.top_k_input = gr.Slider(
+                    minimum=1,
+                    maximum=100,
+                    step=1,
+                    value=40,
+                    label='Top-K',
+                    interactive=True
+                )
+                self.top_p_input = gr.Slider(
+                    minimum=0.0,
+                    maximum=1.0,
+                    step=0.1,
+                    value=0.9,
+                    label='Top-P',
+                    interactive=True
+                )
             
     def _events(self):
         self.msg.submit(
             fn=self.response,
-            inputs=[self.msg, self.chat_history_state, self.system_prompt_state, self.model_name_state],
+            inputs=[self.msg, self.chat_history_state, self.system_prompt_input, self.model_name_input, 
+                    self.temperature_input, self.top_k_input, self.top_p_input],
             outputs= [self.msg, self.chat_history_state, self.chatbot],
         )
         self.undo_btn.click(
@@ -186,45 +232,49 @@ class ChatUI:
         )
         self.regen_btn.click(
             fn = self._regenerate_last_message,
-            inputs=[self.chat_history_state, self.system_prompt_state, self.model_name_state],
+            inputs=[self.chat_history_state, self.system_prompt_input, self.model_name_input,
+                    self.temperature_input, self.top_k_input, self.top_p_input],
             outputs= [self.msg, self.chat_history_state, self.chatbot],
         )
         self.save_chat_btn.click(
             fn=self._save_chat,
-            inputs=[self.chat_history_state, self.chat_files_dir, self.new_chat_name],
+            inputs=[self.chat_history_state, self.chat_files_dir_input, self.new_chat_name],
             outputs=[self.new_chat_name, self.chat_files_dropdown]
         )
         self.load_chat_btn.click(
             fn=self._load_chat,
-            inputs=[self.chat_files_dir, self.chat_files_dropdown],
+            inputs=[self.chat_files_dir_input, self.chat_files_dropdown],
             outputs=[self.chat_history_state, self.chatbot, self.new_chat_name]
         )
         self.new_chat_btn.click(
             fn=self._create_new_chat,
-            inputs=[self.new_chat_name, self.chat_files_dir],
-            outputs=[self.chat_history_state, self.chatbot, self.chat_files_dropdown]
+            inputs=[self.chat_files_dir_input, self.new_chat_name],
+            outputs=[self.chat_history_state, self.chatbot, self.chat_files_dropdown, self.new_chat_name]
         )
         self.delete_chat_btn.click(
             fn=self._delete_chat,
-            inputs=[self.chat_files_dropdown, self.chat_files_dir,self.chat_history_state],
+            inputs=[self.chat_files_dir_input, self.chat_files_dropdown, self.chat_history_state],
             outputs=[self.chat_history_state, self.chatbot, self.chat_files_dropdown, self.new_chat_name]
         )
-        self.update_chat_dir_btn.click(
-            fn=self.parse_json_files,
-            inputs=self.chat_files_dir,
+        self.chat_files_dir_input.submit(
+            fn=lambda chat_files_dir: gr.Dropdown(choices=self._parse_chat_folder(chat_files_dir)),
+            inputs=self.chat_files_dir_input,
             outputs=self.chat_files_dropdown,
         )
         
-        self.update_settings_btn.click(
-            fn=self._update_settings,
-            inputs=[self.system_prompt_input],
-            outputs=[self.system_prompt_state]
+        self.chat_files_dir_input.submit(
+            fn=lambda chat_diles_dir: print(f'Chat Files Directory set to: {chat_diles_dir}'),
+            inputs=[self.chat_files_dir_input]
+        )
+        
+        self.system_prompt_input.submit(
+            fn=lambda system_prompt: print(f'System Prompt set to: {system_prompt}'),
+            inputs=[self.system_prompt_input]
         )
         
         self.model_name_input.change(
-            fn=self._update_model,
+            fn=lambda model_name: print(f'Model name set to: {model_name}'),
             inputs=[self.model_name_input],
-            outputs=[self.model_name_state]
         )
 
 with gr.Blocks() as demo:
