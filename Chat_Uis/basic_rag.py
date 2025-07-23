82de88dbf9c34cc012b1a7165d4dcef0ed8479bf
Sushant Singh <151197353+SushantSingh-23-01@users.noreply.github.com>
2025-07-23 18:23:27 +0530

    Update basic_rag.py

--- Chat_Uis/basic_rag.py
+++ Chat_Uis/basic_rag.py
@@ -7,6 +7,8 @@ from dataclasses import dataclass
 from PyPDF2 import PdfReader
 import chromadb
 from chromadb.config import Settings
+import re
+import subprocess
 
 client = chromadb.Client(Settings(anonymized_telemetry=False))
 chroma_collection = client.get_or_create_collection('docs')
@@ -23,7 +25,7 @@ class SharedState:
     top_k:int = 50
     top_p:float = 0.9
 
-    enable_rag:bool = False 
+    enable_rag:bool = False
 
 def save_chat(history, filename, shared_state):
     if not history:
@@ -77,16 +79,53 @@ def simple_text_splitter(text, chunk_size=256, chunk_overlap=64):
             chunks.append(text[i: i + chunk_size])
     return chunks
 
-def embed_pdfs(files, model_config, chunk_size, chunk_overlap):
+def simple_paragraph_splitter(text, max_chunk_chars, overlap_chars):
+    paragraphs = re.split(r'\n\n+', text) # split of one or more newlines
+    
+    chunks = []
+    current_chunk = ""
+    
+    for para in paragraphs:
+        para = para.strip()
+        if not para:
+            continue
+        
+        # If adding the next paragraph exceeds max_chunk_chars, close current chunk and start new
+        # +2 for potential newline
+        if len(current_chunk) + len(para) + 2 > max_chunk_chars and current_chunk:
+            chunks.append(current_chunk)
+            
+            # Create overlap: take last 'overlap_chars' from current_chunk
+            if len(current_chunk) >= overlap_chars:
+                current_chunk = current_chunk[-overlap_chars:].strip()
+            else: 
+                current_chunk = ""
+            
+        if current_chunk:
+            current_chunk += "\n\n" + para
+        else:
+            current_chunk = para
+
+    if current_chunk: # Add the last chunk if any remaining
+        chunks.append(current_chunk)
+    
+    return chunks
+
+def embed_pdfs(files, model_config, chunk_size, chunk_overlap, split_tech):
     if files is None:
         return gr.Textbox('upload pdf file')
-    
+    char_count = 0
     chunk_count = 0
     for f in files:
         reader = PdfReader(f)
         for i, page in enumerate(reader.pages):
             text = page.extract_text().strip()
-            chunks = simple_text_splitter(text, chunk_size, chunk_overlap)
+            char_count += len(text)
+            if split_tech == 'simple':
+                chunks = simple_text_splitter(text, chunk_size, chunk_overlap)
+            else:
+                chunks = simple_paragraph_splitter(text, chunk_size, chunk_overlap)
+            
             for j, chunk in enumerate(chunks):
                 embeddings = ollama.embed(model_config.emb_model, chunk)['embeddings']
                 unique_ids = f'{os.path.basename(f)}_page{i}_chunk_{j}'
@@ -96,7 +135,10 @@ def embed_pdfs(files, model_config, chunk_size, chunk_overlap):
                     documents=chunk
                 )
                 chunk_count += 1
-    return f'Status: Embedded PDFs in ChromaDB. Total chunks count: {chunk_count}'
+                
+    return gr.Textbox(f'Status: Embedded PDFs in ChromaDB.'
+            f'\nTotal Length of Text: {char_count}'
+            f'\nTotal chunks count: {chunk_count}')
 
 def ollama_response(message, history, shared_state):
     system_prompt = shared_state.system_prompt 
@@ -154,8 +196,7 @@ def update_settings(shared_state, chat_model, emb_model, system_prompt, temperat
     shared_state.temperature = temperature
     shared_state.top_k = top_k
     shared_state.top_p = top_p
-    print(f'Settings Updated')
-    return shared_state
+    return shared_state, gr.Textbox('Settings updated')
     
 def clear_rag_embeddings():
     if chroma_collection.count() > 0:
@@ -170,6 +211,67 @@ def available_ollama_models():
     for i in models_info['models']:
         model_names.append(i['model'])
     return model_names
+
+def pull_ollama_model(name):
+    command = ['ollama', 'pull', name]
+    try:
+        process = subprocess.run(command, check=True)
+        msg = f'Successfully Pulled {name}'
+    except subprocess.CalledProcessError as e:
+        print('Error:', e)
+        msg = f'Failed To load Model.'
+    except FileNotFoundError:
+        msg = f'Ollama not found.'
+    return (gr.Dropdown(available_ollama_models()), gr.Dropdown(available_ollama_models()), 
+             gr.Dropdown(available_ollama_models()), gr.Textbox(msg))
+
+def delete_ollama_model(name):
+    command = ['ollama', 'rm', name]
+    try:
+        process = subprocess.run(command, check=True)
+        msg = f'Successfully Removed {name}'
+    except subprocess.CalledProcessError as e:
+        print('Error:', e)
+        msg = f'Failed To load Model.'
+    except FileNotFoundError:
+        msg = f'Ollama not found.'
+    return (gr.Dropdown(available_ollama_models()), gr.Dropdown(available_ollama_models()), 
+             gr.Dropdown(available_ollama_models()), gr.Textbox(msg))
+    
+def save_profile(shared_state, profile_name):
+    if profile_name:
+        data = {
+            'model_name': shared_state.chat_model,
+            'system_prompt': shared_state.system_prompt,
+            'temperature': shared_state.temperature,
+            'top_k': shared_state.top_k,
+            'top_p': shared_state.top_p
+        }
+        filepath = os.path.join(shared_state.cwd, 'profiles', profile_name)
+        filepath = filepath + '.json' if not filepath.endswith('json') else filepath
+        with open(filepath, 'w', encoding='utf-8') as f:
+            json.dump(data, f, indent=4)
+        msg = f'Saved profile :{profile_name}'
+    else:
+        msg = 'Provide File Name'
+    update_profile_list = [f for f in os.listdir(os.path.join(shared_state.cwd, 'profiles')) if f.endswith('json')]
+    return gr.Dropdown(update_profile_list), gr.Textbox(msg)
+
+def load_profile(shared_state, profile_name):
+    filepath = os.path.join(shared_state.cwd, 'profiles', profile_name)
+    with open(filepath, 'r', encoding='utf-8') as f:
+        data = json.load(f)
+
+    if data:
+        shared_state.chat_model = data['chat_model']
+        shared_state.system_prompt = data['system_prompt']
+        shared_state.temperature = data['temperature']
+        shared_state.top_k = data['top_k']
+        shared_state.top_p = data['top_p']
+        msg = 'Loaded Profile'
+    else:
+        msg = 'Profile Corrupted/Empty'
+    return shared_state, gr.Textbox(msg)
 
 class ChatUI:
     def __init__(self, shared_state):
@@ -223,61 +325,96 @@ class ChatUI:
                     label='PDF file',
                 )  
                 with gr.Row():
-                    self.chunk_size = gr.Slider(minimum=128, maximum=1024, value=256, 
+                    self.chunk_size = gr.Slider(minimum=128, maximum=1024, value=512, 
                                                 step=1, label='Chunk Size', interactive=True)
                     self.chunk_overlap = gr.Slider(minimum=0, maximum=128, value=64, 
                                                 step=1, label='Chunk Overlap', interactive=True)
+                    self.split_tech = gr.Dropdown(
+                        choices=['simple', 'paragraph'],
+                        value='paragraph',
+                        label='Splitting Technique',
+                        interactive=True
+                    )
                 with gr.Row():
                     self.process_pdf_btn = gr.Button('Process PDF', interactive=True)
                     self.clear_embds_btn = gr.Button('Delete Embeddings', interactive=True)
-                self.pdf_status = gr.Textbox(label='pdf status', interactive=False)
+                self.pdf_status = gr.Textbox(label='pdf status', interactive=False, lines=3)
                 
         # Settings Page
         with gr.Column(visible=False) as self.settings_page:
             gr.Markdown("### Settings")
-            self.chat_models_dropdown = gr.Dropdown(
-                available_ollama_models(),
-                label='Choose Chat Model',
-                interactive=True,
-            )
-            self.emb_models_dropdown = gr.Dropdown(
-                available_ollama_models(),
-                label='Choose Embedding Model',
-                interactive=True,
-            )
-            self.system_prompt_in = gr.Textbox(
-                value='Act as a helpful assistant',
-                label='Chat Model System Prompt',
-                interactive=True, 
-            )
+            with gr.Accordion('Checkpoints Selection', open=False):
+                self.chat_models_dropdown = gr.Dropdown(
+                    available_ollama_models(),
+                    label='Choose Chat Model',
+                    interactive=True,
+                )
+                self.emb_models_dropdown = gr.Dropdown(
+                    available_ollama_models(),
+                    label='Choose Embedding Model',
+                    interactive=True,
+                )
             
-            with gr.Row():
-                self.temp_in = gr.Slider(
-                    minimum=0,
-                    maximum=1.0,
-                    step=0.1,
-                    value=0.9,
-                    label='Temperature',
-                    interactive=True
+            with gr.Accordion('Model Settings', open=False):
+                with gr.Row():
+                    self.profile_dropdown = gr.Dropdown(
+                        choices=[f for f in os.listdir('profiles') if f.endswith('json')],
+                        label='Available Profiles',
+                        interactive=True
+                    )
+                    self.load_profile_btn = gr.Button('Load Profile')
+                self.system_prompt_in = gr.Textbox(
+                    value='Act as a helpful assistant',
+                    label='Chat Model System Prompt',
+                    interactive=True, 
                 )
-                self.top_k_in = gr.Slider(
-                    minimum=1,
-                    maximum=100,
-                    step=1,
-                    value=40,
-                    label='Top-K',
-                    interactive=True
-                )
-                self.top_p_in = gr.Slider(
-                    minimum=0.0,
-                    maximum=1.0,
-                    step=0.1,
-                    value=0.9,
-                    label='Top-P',
-                    interactive=True
-                )
-            self.update_settings_btn = gr.Button('Update Model Settings', interactive=True)
-
+                
+                with gr.Row():
+                    self.temp_in = gr.Slider(
+                        minimum=0,
+                        maximum=1.0,
+                        step=0.1,
+                        value=0.9,
+                        label='Temperature',
+                        interactive=True
+                    )
+                    self.top_k_in = gr.Slider(
+                        minimum=1,
+                        maximum=100,
+                        step=1,
+                        value=40,
+                        label='Top-K',
+                        interactive=True
+                    )
+                    self.top_p_in = gr.Slider(
+                        minimum=0.0,
+                        maximum=1.0,
+                        step=0.1,
+                        value=0.9,
+                        label='Top-P',
+                        interactive=True
+                    )
+                with gr.Row():
+                    self.new_profile_in = gr.Textbox(label='New Profile Name', interactive=True)
+                    self.save_profile_btn = gr.Button('save Profile')
+                    
+            with gr.Accordion('Model Downloading and Removing', open=False):
+                with gr.Row():
+                    self.download_model_in = gr.Textbox(
+                        label='Checkpoint Name/ HF Directory',
+                        placeholder='hf.co/ggml-org/SmolLM3-3B-GGUF:Q4_K_M',
+                        interactive=True, 
+                    )
+                    self.download_model_btn = gr.Button('Download Model', interactive=True)
+                with gr.Row():
+                    self.delete_model_in = gr.Dropdown(
+                        choices=available_ollama_models(),
+                        label='Name of checkpoint to delete',
+                        interactive=True
+                    )
+                    self.delete_model_btn = gr.Button('Delete model', interactive=True)
+            self.update_settings_btn = gr.Button('Update Model Settings', size='md',interactive=True)
+            self.settings_status = gr.Textbox(label='Status', interactive=False)
 
                 
     def _events(self):
@@ -296,8 +433,8 @@ class ChatUI:
         
         self.process_pdf_btn.click(
             fn=embed_pdfs,
-            inputs=[self.pdf_path, self.shared_state, 
-                    self.chunk_size, self.chunk_overlap],
+            inputs=[self.pdf_path, self.shared_state, self.chunk_size, 
+                    self.chunk_overlap, self.split_tech],
             outputs=[self.pdf_status]
         )
         self.msg.submit(
@@ -339,13 +476,35 @@ class ChatUI:
             update_settings,
             inputs=[self.shared_state, self.chat_models_dropdown, self.emb_models_dropdown,
                     self.system_prompt_in, self.temp_in, self.top_k_in, self.top_p_in],
-            outputs=[self.shared_state]
+            outputs=[self.shared_state, self.settings_status]
         )
         self.clear_embds_btn.click(
             clear_rag_embeddings,
             outputs=[self.pdf_status]
         )
-        
+        self.download_model_btn.click(
+            pull_ollama_model,
+            [self.download_model_in],
+            [self.chat_models_dropdown, self.emb_models_dropdown, 
+             self.delete_model_in, self.settings_status]
+        )
+        self.delete_model_btn.click(
+            delete_ollama_model,
+            [self.delete_model_in],
+            [self.chat_models_dropdown, self.emb_models_dropdown, 
+             self.delete_model_in, self.settings_status]
+        )
+        self.load_profile_btn.click(
+            load_profile,
+            [self.shared_state, self.profile_dropdown],
+            [self.shared_state, self.settings_status]
+        )
+        self.save_profile_btn.click(
+            save_profile,
+            [self.shared_state, self.new_profile_in],
+            [self.profile_dropdown, self.settings_status]
+        )
+
 with gr.Blocks() as demo:
     app_shared_state = gr.State(SharedState())
     ChatUI(app_shared_state)
